55e9de1c7e52c9daec7d275e4c50688444ec44a9
henry16lin <47073122+henry16lin@users.noreply.github.com>
2019-08-14 16:05:47 +0800

    Update train.py
    output model structure json file

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint,TensorBoard,ReduceLROnPlateau
 from keras.models import load_model
 
 import model_structure
@@ -50,14 +50,22 @@ def training():
     filepath= os.path.join(cwd,'checkpoint','ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5')
     logging = TensorBoard(log_dir=os.path.join(cwd,'checkpoint'))
     checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,save_weights_only=True, save_best_only=True, mode='min')
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1)
-    callbacks_list = [checkpoint,logging,reduce_lr]
+    #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1)
+    callbacks_list = [checkpoint,logging]
     
     # Fit the model
     train_history = model.fit(x=x_train_normalize,
                               y=y_train_one_hot,
                               validation_split=0.1,epochs=epochs,batch_size=batch_size,
                               callbacks=callbacks_list, verbose=1)
+    
+    # save model structure
+    with open(os.path.join(cwd,'checkpoint',"model_structure.json"), "w") as json_file:
+        json_file.write(model.to_json())
+    
+    # save final epoch model weight
+    model.save_weights(os.path.join(cwd,'checkpoint','trained_weights_final_epoch.h5'))
+    
     
     show_train_history(train_history,'acc','val_acc')
     show_train_history(train_history,'loss','val_loss')
